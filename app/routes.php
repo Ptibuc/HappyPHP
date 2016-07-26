a9264cbf2fc210873848d1b21000184e0d767707
Bucaille <Bucaille>
2016-07-26 16:43:24 +0200

    maj 26072016 16h43

--- app/routes.php
+++ app/routes.php
@@ -2,11 +2,5 @@
 
 // Home page
 $app->get('/', function () {
-    require '../src/model.php';
-    $articles = getArticles();
-
-    ob_start();             // start buffering HTML output
-    require '../views/view.php';
-    $view = ob_get_clean(); // assign HTML output to $view
-    return $view;
+    echo 'prout';
 });
